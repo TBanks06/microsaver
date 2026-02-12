8a0d32529528dab29517f8787b6207f7c1b09c89
Tunji Bankole <shubbywax@gmail.com>
2026-02-12 01:49:39 +0100

    change webdir to dist

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.wayoutmobility.microsaver',
   appName: 'Microsaver',
-  webDir: 'build'
+  webDir: 'dist'
 };
 
 export default config;
